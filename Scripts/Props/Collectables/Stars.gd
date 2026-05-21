160e8526cd31f4cb9748975f8be7ef03e9b684af
Assani <23000349+sassani134@users.noreply.github.com>
2026-05-21 18:59:12 +0200

    [WIP] Working on the Collectables Interactables Triggers - Focus on Star for level

--- Scripts/Props/Collectables/Stars.gd
+++ Scripts/Props/Collectables/Stars.gd
@@ -1,11 +1,20 @@
-extends Collectables
+extends Collectable
 
-var star_name: String
+@export var star_name: String
 var star_position: Vector3
-var star_enabled: bool
+var star_enabled: bool = true
+
+# signal star_collected(star_name)
 
 # When grabed emit a signal to the level that it was grabed
 # change SaveloadManager for this level and star name
 # pass it to true so it save the star
 # exit the level
 # the display a modal that display the star collected the score coins and if you want to save or continue
+
+
+func _ready() -> void:
+    pass
+
+func _on_area_entered(area: Area3D) -> void:
+    pass
